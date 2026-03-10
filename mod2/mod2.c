1446a9cbc6d9f55da6bb3586c2821209825fff59
202508915197 <202508915197@alunos.estacio.br>
2026-03-10 00:27:23 +0000

    /fim

--- mod2/mod2.c
+++ mod2/mod2.c
@@ -10,10 +10,9 @@
 
 //estrutura
 
-struct 
-{
+struct Livro {
     char nome[TAM_STRING];
-    char nome[TAM_STRING];
+    char autor[TAM_STRING];
     char editora[TAM_STRING];
     int edicao;
 };
@@ -23,6 +22,7 @@ struct
 void limparBufferEntrada() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF)
+        ;
 }
 
 //funcao principal
@@ -32,86 +32,90 @@ int main() {
     int opcao;
 
     //laco principal menu
-do {
-    //exibe menu de opcoes
-    printf("**************************")
-    printf("        BIBLIOTECA        ")
-    printf("**************************")
-    printf("1 - CADASTRAR NOVO LIVRO  ")
-    printf("2 - LISTAR TODOS OS LIVROS")
-    printf("0 - SAIR                  ")
-    printf("**************************")
-    printf("ESCOLHA UMA OPCAO:        ")
+    do {
+        //exibe menu de opcoes
+        printf("**************************\n");
+        printf("        BIBLIOTECA        \n");
+        printf("**************************\n");
+        printf("1 - CADASTRAR NOVO LIVRO  \n");
+        printf("2 - LISTAR TODOS OS LIVROS\n");
+        printf("0 - SAIR                  \n");
+        printf("**************************\n");
+        printf("ESCOLHA UMA OPCAO:        ");
 
-    //le a opcao do usuario
-    scanf("%d, &opcao");
-    limparBufferEntrada(); //limpa o '\n' deixado pelo scanf
-
-    //processamento da opcao
-switch (opcao) {
-    case 1 //cadastro de livro
-        printf("***CADASTRO DE NOVO LIVRO***\n\n");
-
-        if (totalLivros < MAX_LIVROS) {
-            printf("DIGITE O NOME DO LIVRO:");
-            fgets(biblioteca[totalLivros].nome, TAM_STRING, stdin);
-
-            printf("DIGITE O AUTOR:");
-            fgets(biblioteca[totalLivros].autor, TAM_STRING, stdin);
-
-            printf("DIGITE A EDITORA:");
-            fgets(biblioteca[totalLivros].editora, TAM_STRING, stdin);
-        
-            biblioteca[totalLivros].nome[strcspn(biblioteca[totalLivros].nome, "\n") = '\n']
-            biblioteca[totalLivros].autor[strcspn(biblioteca[totalLivros].autor, "\n") = '\n']
-            biblioteca[totalLivros].editora[strcspn(biblioteca[totalLivros].editora, "\n") = '\n']
-
-            printf("DIGITE A EDICAO:");
-            scanf("%d", &biblioteca[totalLivros].edicao);
+        //le a opcao do usuario
+        if (scanf("%d", &opcao) != 1) {
             limparBufferEntrada();
+            opcao = -1;
+        }
+        limparBufferEntrada(); //limpa o '\n' deixado pelo scanf
 
-            //totalLivros = totalLivros + 1;
-            totalLivros++;
+        //processamento da opcao
+        switch (opcao) {
+            case 1: //cadastro de livro
+                printf("***CADASTRO DE NOVO LIVRO***\n\n");
 
-            printf("\nLIVRO CADASTRADO COM SUCESSO!\n");
-        } else {
-            printf("BIBLIOTECA CHEIA! NAO E POSSIVEL CADASTRAR MAIS LIVROS.\n");
+                if (totalLivros < MAX_LIVROS) {
+                    printf("DIGITE O NOME DO LIVRO: ");
+                    fgets(biblioteca[totalLivros].nome, TAM_STRING, stdin);
+
+                    printf("DIGITE O AUTOR: ");
+                    fgets(biblioteca[totalLivros].autor, TAM_STRING, stdin);
+
+                    printf("DIGITE A EDITORA: ");
+                    fgets(biblioteca[totalLivros].editora, TAM_STRING, stdin);
+
+                    biblioteca[totalLivros].nome[strcspn(biblioteca[totalLivros].nome, "\n")] = '\0';
+                    biblioteca[totalLivros].autor[strcspn(biblioteca[totalLivros].autor, "\n")] = '\0';
+                    biblioteca[totalLivros].editora[strcspn(biblioteca[totalLivros].editora, "\n")] = '\0';
+
+                    printf("DIGITE A EDICAO: ");
+                    scanf("%d", &biblioteca[totalLivros].edicao);
+                    limparBufferEntrada();
+
+                    totalLivros++;
+
+                    printf("\nLIVRO CADASTRADO COM SUCESSO!\n");
+                } else {
+                    printf("BIBLIOTECA CHEIA! NAO E POSSIVEL CADASTRAR MAIS LIVROS.\n");
+                }
+                break;
+
+            case 2: //listagem de livro
+                printf("***LISTA DE LIVROS CADASTRADOS***\n\n");
+
+                if (totalLivros == 0){
+                    printf("NENHUM LIVRO CADASTRADO\n");
+                } else {
+                    for (int i = 0; i < totalLivros; i++) {
+                        printf("***********************************\n");
+                        printf("LIVRO %d\n", i + 1);
+                        printf("NOME %s\n", biblioteca[i].nome);
+                        printf("AUTOR %s\n", biblioteca[i].autor);
+                        printf("EDITORA %s\n", biblioteca[i].editora);
+                        printf("EDICAO %d\n", biblioteca[i].edicao);
+                    }
+                    printf("***********************************\n");
+                }
+
+                //a pausa e crucial para que o usuario veja a lista
+                //do proximo loop limpar a tela
+                printf("\nPRECIONE ENTER PARA CONTINUAR...");
+                getchar();
+                break;
+
+            case 0: //sair
+                printf("\nSAINDO DO SISTEMA...\n");
+                break;
+
+            default: // opcao invalida
+                printf("\nOPCAO INVALIDA TENTE NOVAMENTE.\n");
+                printf("\nPRESSIONE ENTER PARA CONTINUAR...");
+                getchar();
+                break;
         }
 
-    case 2 //listagem de livro
-        printf("***LISTA DE LIVROS CADASTRADOS***\n\n");
-
-        if (totalLivros == 0){
-            printf("NENHUM LIVRO CADASTRADO");
-        } else {
-            for (int i = 0; i < totalLivros; i++) {
-                printf("***********************************");
-                printf("LIVRO %d\n", i + 1);
-                printf("NOME %s\n", biblioteca[i].nome);
-                printf("AUTOR %s\n", biblioteca[i].autor);
-                printf("EDITORA %s\n", biblioteca[i].editora);
-                printf("EDICAO %d\n", biblioteca[i].edicao);
-            }
-            printf("***********************************");
-        }
-        //a pausa e crucial para que o usuario veja a lista
-        //do proximo loop limpar a tela
-        printf("\nPRECIONE ENTER PARA CONTINUAR...");
-        getchar();
-        break;
-
-    case 0: //sair
-    printf("\nSAINDO DO SISTEMA...");
-    break;
-
-    default: // opcao invalida
-        printf("\nOPCAO INVALIDA TENTE NOVAMENTE.\n")
-        printf("\nPRESSIONE ENTER PARA CONTINUAR...")
-        getchar();
-        break;
-        }   
-
-} while (opcao != 0);
+    } while (opcao != 0);
 
     return 0; //fim do programa
 }
