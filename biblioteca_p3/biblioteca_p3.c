03c971c76781f0b53efc8c09c40e1be0f900aa1f
202508915197 <202508915197@alunos.estacio.br>
2026-03-17 23:57:31 +0000

    biblioteca_p3

---
+++ biblioteca_p3/biblioteca_p3.c
@@ -0,0 +1,11 @@
+/*
+continuidade ao sistema da biblioteca, mas agora no nível mestre, 
+aplicando modularização do código com funções especializadas. 
+Serão utilizados ponteiros, 
+alocação dinâmica com malloc, calloc e free, 
+além da passagem por valor para exibição de dados e 
+por referência para atualizar empréstimos, 
+consolidando boas práticas de estruturação e 
+manipulação de memória em C.
+*/
+
