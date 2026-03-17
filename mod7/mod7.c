4e64b4ace0cb31f50b5af63bfcd045d147e3fcb6
202508915197 <202508915197@alunos.estacio.br>
2026-03-17 00:04:26 +0000

    bibliotecas

---
+++ mod7/mod7.c
@@ -0,0 +1,14 @@
+/*
+
+<stdio.h>	Entrada e saída padrão (ex: printf, scanf)
+<stdlib.h>	Funções utilitárias (ex: malloc, free, exit)
+<string.h>	Manipulação de strings (ex: strlen, strcpy, strcmp)
+<math.h>	Funções matemáticas (ex: sqrt, pow, sin)
+<time.h>	Controle de tempo e data (ex: time, clock, difftime)
+<ctype.h>	Testes de caracteres (ex: isalpha, isdigit, toupper)
+<stdbool.h>	Permite usar o tipo bool com true e false
+<limits.h>	Define limites de tipos primitivos (INT_MAX, CHAR_MIN, etc.)
+<float.h>	Define limites para tipos de ponto flutuante (FLT_MAX, etc.)
+
+*/
+
